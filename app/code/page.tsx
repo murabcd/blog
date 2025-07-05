55abf0e008a2a04006fd9bbc25255d391e387e17
muradpm <abdulkadyr0v@yandex.ru>
2025-07-05 23:54:23 +0300

    add articles

--- app/code/page.tsx
+++ app/code/page.tsx
@@ -40,6 +40,16 @@ export default function Page() {
             Vibe Coder
           </Link>
         </li>
+        <li>
+          <Link
+            href="https://github.com/murabcd/compass"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline"
+          >
+            AI Recruiter
+          </Link>
+        </li>
       </ul>
     </section>
   );
